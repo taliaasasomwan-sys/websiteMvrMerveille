99ec04168ddd155d281d790cc18efc151d85200e
Talia Asasomwan <talia.asasomwan@bazandpoort.be>
2026-04-30 13:50:23 +0200

    resp design fix

--- hoofdpagina/script.js
+++ hoofdpagina/script.js
@@ -1,4 +1,14 @@
-function toggleMenu() {
-  const nav = document.getElementById("nav");
-  nav.classList.toggle("active");
+document.addEventListener("DOMContentLoaded", function() {
+function openImage(img) {
+  const lightbox = document.getElementById('lightbox');
+  const lightboxImg = document.getElementById('lightbox-img');
+  lightboxImg.src = img.src;
+  lightboxImg.alt = img.alt;
+  lightbox.classList.add('open');
 }
+
+function closeLightbox() {
+  document.getElementById('lightbox').classList.remove('open');
+}
+
+});
